46e46aca12722099e8c059ca4198fd453dc76f84
YeonghunKO <yhko1988@gmail.com>
2022-11-29 21:42:57 +0900

    feat : arrow 추가

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,5 +3,6 @@ import Bag from './Bag';
 import Plus from './Plus';
 import User from './User';
 import X from './X';
+import Arrow from './Arrow';
 
-export default { Cart, Bag, Plus, User, X };
+export default { Cart, Bag, Plus, User, X, Arrow };
